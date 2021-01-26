3c9becc02ae9ac75897fa5697669e954e03831b1
Crunch Rock <lordpiraterock@outlook.com>
2021-01-26 06:41:52 -0500

    added the new pixel art gallery and cyber whiskey prologue sections

--- js/header.js
+++ js/header.js
@@ -1,13 +1,11 @@
 const updatePadding = () => {
-let header = document.querySelector("header");
-let style = getComputedStyle(header);
-let headerHeight = style.height;
-let wrapper = document.getElementById("top-image");
-setTimeout(console.log(wrapper), 3000)
-let wrapperStyle = getComputedStyle(wrapper);
-let wrapperPadding = wrapperStyle.paddingTop;
-setTimeout(console.log(wrapperPadding), 3000)
-document.getElementById("top-image").style.paddingTop = headerHeight;
+    let header = document.querySelector("header");
+    let style = getComputedStyle(header);
+    let headerHeight = style.height;
+    let wrapper = document.getElementById("top-image");
+    let wrapperStyle = getComputedStyle(wrapper);
+    let wrapperPadding = wrapperStyle.paddingTop;
+    document.getElementById("top-image").style.paddingTop = headerHeight;
 }
 
 setInterval(updatePadding, 500);
